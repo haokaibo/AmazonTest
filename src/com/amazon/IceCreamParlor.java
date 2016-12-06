109db91e58101e29783d710925071fab022c7b15
Kevin Hao <kaibohao@amazon.com>
2016-12-06 19:11:21 +0800

    remove empty line

--- src/com/amazon/IceCreamParlor.java
+++ src/com/amazon/IceCreamParlor.java
@@ -32,7 +32,6 @@ public class IceCreamParlor {
         return -1;
     }
 
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
